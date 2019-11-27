1b53d21114afbbe01b9f3d61c1eaf5529196a979
Robert <geicarobert@gmail.com>
2019-11-27 17:58:02 +0200

    add second dice

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-let output = document.getElementById("dice");
-// create a new element for images
-let img = document.createElement('img');
+let dice1 = document.getElementById('dice1');
+let dice2 = document.getElementById('dice2');
+// let result = document.getElementById('result');
 
-function getRandomDice() {
+
+function getRandomDices() {
+    randomDice(dice1);
+    randomDice(dice2);
+}
+
+function randomDice(dice) {
+    // check if dice has been generated
+    if (dice.hasChildNodes()) {
+        // if true, then delete dices to avoid duplication
+        dice.removeChild(dice.childNodes[0]);
+    } 
+
     // random number from 1 to 6
     let randomNum = Math.floor(Math.random() * 6) + 1;
-
+    // create a new element for images
+    let img = document.createElement('img');
     // get random dice images with dom manipulation
-    img.src = 'images/dice' + randomNum + '.png';
+    img.src = 'img/dice' + randomNum + '.png';
 
-    // output result
-    output.appendChild(img);
+    dice.appendChild(img);
 
-    console.log(output.appendChild(img))
+
+
+    // fix total score
+    console.log(randomNum);
+    // result.innerHTML = randomNum;
 }
